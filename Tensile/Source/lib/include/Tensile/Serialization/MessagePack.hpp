f7d7d12d3e39243ecd6bb227e92a8a07369e8814
Lee Killough <killough@leekillough.com>
2020-05-21 04:53:22 -0400

    reformatting

--- Tensile/Source/lib/include/Tensile/Serialization/MessagePack.hpp
+++ Tensile/Source/lib/include/Tensile/Serialization/MessagePack.hpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2020 Advanced Micro Devices, Inc.
+ * Copyright 2020 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -28,8 +28,8 @@
 
 #include <type_traits>
 
-#include <Tensile/Serialization.hpp>
 #include <Tensile/ContractionLibrary.hpp>
+#include <Tensile/Serialization.hpp>
 
 #include <msgpack.hpp>
 
@@ -43,31 +43,69 @@ namespace Tensile
             static const bool value = false;
         };
 
-        template <> struct is_Convertible<std::string> { static const bool value = true; };
-        template <> struct is_Convertible<int8_t>      { static const bool value = true; };
-        template <> struct is_Convertible<int16_t>     { static const bool value = true; };
-        template <> struct is_Convertible<int32_t>     { static const bool value = true; };
-        template <> struct is_Convertible<int64_t>     { static const bool value = true; };
-        template <> struct is_Convertible<size_t>      { static const bool value = true; };
-        template <> struct is_Convertible<float>       { static const bool value = true; };
-        template <> struct is_Convertible<double>      { static const bool value = true; };
-        template <> struct is_Convertible<bool>        { static const bool value = true; };
+        template <>
+        struct is_Convertible<std::string>
+        {
+            static const bool value = true;
+        };
+        template <>
+        struct is_Convertible<int8_t>
+        {
+            static const bool value = true;
+        };
+        template <>
+        struct is_Convertible<int16_t>
+        {
+            static const bool value = true;
+        };
+        template <>
+        struct is_Convertible<int32_t>
+        {
+            static const bool value = true;
+        };
+        template <>
+        struct is_Convertible<int64_t>
+        {
+            static const bool value = true;
+        };
+        template <>
+        struct is_Convertible<size_t>
+        {
+            static const bool value = true;
+        };
+        template <>
+        struct is_Convertible<float>
+        {
+            static const bool value = true;
+        };
+        template <>
+        struct is_Convertible<double>
+        {
+            static const bool value = true;
+        };
+        template <>
+        struct is_Convertible<bool>
+        {
+            static const bool value = true;
+        };
 
         std::map<std::string, msgpack::object> objectToMap(msgpack::object& object);
 
         struct MessagePackInput
         {
-            msgpack::object object;
+            msgpack::object          object;
             std::vector<std::string> error;
 
             std::set<std::string> usedKeys;
-            int enumFound = 0;
+            int                   enumFound = 0;
 
-            void * context = nullptr;
+            void* context = nullptr;
 
-            MessagePackInput(msgpack::object object, void * context = nullptr)
-            : object(object), context(context)
-            { }
+            MessagePackInput(msgpack::object object, void* context = nullptr)
+                : object(object)
+                , context(context)
+            {
+            }
 
             MessagePackInput createSubRef(msgpack::object otherObject)
             {
@@ -75,14 +113,14 @@ namespace Tensile
             }
 
             template <typename T>
-            void mapRequired(const char * key, T & obj)
+            void mapRequired(const char* key, T& obj)
             {
                 auto map = objectToMap(object);
 
                 auto iterator = map.find(key);
-                if (iterator != map.end())
+                if(iterator != map.end())
                 {
-                    auto& value = iterator->second;
+                    auto&            value  = iterator->second;
                     MessagePackInput subRef = createSubRef(value);
                     subRef.input(obj);
                     error.insert(error.end(), subRef.error.begin(), subRef.error.end());
@@ -95,12 +133,12 @@ namespace Tensile
             }
 
             template <typename T>
-            void mapOptional(const char * key, T & obj)
+            void mapOptional(const char* key, T& obj)
             {
                 auto map = objectToMap(object);
 
                 auto iterator = map.find(key);
-                if (iterator != map.end())
+                if(iterator != map.end())
                 {
                     auto& value = iterator->second;
                     createSubRef(value).input(obj);
@@ -109,36 +147,38 @@ namespace Tensile
             }
 
             template <typename T>
-            void input(T & obj)
+            void input(T& obj)
             {
                 EmptyContext ctx;
                 input(obj, ctx);
             }
 
             template <typename T, typename Context>
-            typename std::enable_if<has_MappingTraits<T, MessagePackInput, Context>::value, void>::type
-            input(T & obj, Context & ctx)
+            typename std::enable_if<has_MappingTraits<T, MessagePackInput, Context>::value,
+                                    void>::type
+                input(T& obj, Context& ctx)
             {
                 MappingTraits<T, MessagePackInput, Context>::mapping(*this, obj, ctx);
             }
 
             template <typename T, typename Context>
-            typename std::enable_if<has_EmptyMappingTraits<T, MessagePackInput, Context>::value, void>::type
-            input(T & obj, Context & ctx)
+            typename std::enable_if<has_EmptyMappingTraits<T, MessagePackInput, Context>::value,
+                                    void>::type
+                input(T& obj, Context& ctx)
             {
                 MappingTraits<T, MessagePackInput, Context>::mapping(*this, obj);
             }
 
             template <typename T, typename Context>
-            typename std::enable_if<is_Convertible<T>::value, void>::type
-            input(T & obj, Context & ctx)
+            typename std::enable_if<is_Convertible<T>::value, void>::type input(T&       obj,
+                                                                                Context& ctx)
             {
                 object.convert(obj);
             }
 
             template <typename T, typename Context>
             typename std::enable_if<has_EnumTraits<T, MessagePackInput>::value, void>::type
-            input(T & obj, Context & ctx)
+                input(T& obj, Context& ctx)
             {
                 enumFound = 0;
                 EnumTraits<T, MessagePackInput>::enumeration(*this, obj);
@@ -149,15 +189,15 @@ namespace Tensile
 
             template <typename T, typename Context>
             typename std::enable_if<has_SequenceTraits<T, MessagePackInput>::value, void>::type
-            input(T & obj, Context & ctx)
+                input(T& obj, Context& ctx)
             {
                 assert(object.type == msgpack::type::object_type::ARRAY);
                 auto result = object.as<std::vector<msgpack::object>>();
 
-                for (size_t i = 0; i < result.size(); i++)
+                for(size_t i = 0; i < result.size(); i++)
                 {
                     MessagePackInput subRef = createSubRef(result[i]);
-                    auto & value = SequenceTraits<T, MessagePackInput>::element(*this, obj, i);
+                    auto& value = SequenceTraits<T, MessagePackInput>::element(*this, obj, i);
                     subRef.input(value);
 
                     if(!subRef.error.empty())
@@ -170,24 +210,24 @@ namespace Tensile
 
             template <typename T, typename Context>
             typename std::enable_if<has_CustomMappingTraits<T, MessagePackInput>::value, void>::type
-            input(T & obj, Context & ctx)
+                input(T& obj, Context& ctx)
             {
                 auto map = objectToMap(object);
 
-                for (auto & element : map)
+                for(auto& element : map)
                 {
                     CustomMappingTraits<T, MessagePackInput>::inputOne(*this, element.first, obj);
                 }
             }
 
             template <typename T>
-            void enumCase(T & member, const char * key, T value)
+            void enumCase(T& member, const char* key, T value)
             {
                 assert(object.type == msgpack::type::object_type::STR);
                 std::string result;
                 object.convert(result);
 
-                if (result == key)
+                if(result == key)
                 {
                     enumFound++;
                     member = value;
@@ -199,39 +239,39 @@ namespace Tensile
         struct IOTraits<MessagePackInput>
         {
             template <typename T>
-            static void mapRequired(MessagePackInput & io, const char* key, T & obj)
+            static void mapRequired(MessagePackInput& io, const char* key, T& obj)
             {
                 io.mapRequired(key, obj);
             }
 
             template <typename T>
-            static void mapOptional(MessagePackInput & io, const char* key, T & obj)
+            static void mapOptional(MessagePackInput& io, const char* key, T& obj)
             {
                 io.mapOptional(key, obj);
             }
 
-            static bool outputting(MessagePackInput & io)
+            static bool outputting(MessagePackInput& io)
             {
                 return false;
             }
 
-            static void setError(MessagePackInput & io, std::string const& msg)
+            static void setError(MessagePackInput& io, std::string const& msg)
             {
                 io.error.push_back(msg);
             }
 
-            static void setContext(MessagePackInput & io, void * ctx)
+            static void setContext(MessagePackInput& io, void* ctx)
             {
                 io.context = ctx;
             }
 
-            static void * getContext(MessagePackInput & io)
+            static void* getContext(MessagePackInput& io)
             {
                 return io.context;
             }
 
             template <typename T>
-            static void enumCase(MessagePackInput & io, T & member, const char * key, T value)
+            static void enumCase(MessagePackInput& io, T& member, const char* key, T value)
             {
                 io.enumCase(member, key, value);
             }
@@ -240,8 +280,11 @@ namespace Tensile
         template <typename T, typename IO>
         struct SequenceTraits<std::vector<T>, IO>
         {
-            static size_t size(MessagePackInput & io, std::vector<T> & v) { return v.size(); }
-            static T & element(MessagePackInput & io, std::vector<T> & v, size_t index)
+            static size_t size(MessagePackInput& io, std::vector<T>& v)
+            {
+                return v.size();
+            }
+            static T& element(MessagePackInput& io, std::vector<T>& v, size_t index)
             {
                 if(index >= v.size())
                 {
@@ -258,7 +301,7 @@ namespace Tensile
         {
             using obj = MasterContractionLibrary;
 
-            static void mapping(IO & io, std::shared_ptr<obj> & o)
+            static void mapping(IO& io, std::shared_ptr<obj>& o)
             {
                 PointerMappingTraits<obj, IO>::mapping(io, o);
             }
@@ -266,10 +309,10 @@ namespace Tensile
     }
 
     template <typename MyProblem, typename MySolution>
-    std::shared_ptr<SolutionLibrary<MyProblem, MySolution>> MessagePackLoadLibraryFile(std::string const& filename);
+    std::shared_ptr<SolutionLibrary<MyProblem, MySolution>>
+        MessagePackLoadLibraryFile(std::string const& filename);
 
     template <typename MyProblem, typename MySolution>
-    std::shared_ptr<SolutionLibrary<MyProblem, MySolution>> MessagePackLoadLibraryData(std::vector<uint8_t> const& data);
+    std::shared_ptr<SolutionLibrary<MyProblem, MySolution>>
+        MessagePackLoadLibraryData(std::vector<uint8_t> const& data);
 }
-
-
--- Tensile/Source/lib/source/MessagePack.cpp
+++ Tensile/Source/lib/source/MessagePack.cpp
@@ -10,12 +10,12 @@ namespace Tensile
             assert(object.type == msgpack::type::object_type::MAP);
 
             std::map<std::string, msgpack::object> result;
-            for (uint32_t i = 0; i < object.via.map.size; i++)
+            for(uint32_t i = 0; i < object.via.map.size; i++)
             {
-                auto & element = object.via.map.ptr[i];
+                auto& element = object.via.map.ptr[i];
 
                 std::string key;
-                switch (element.key.type)
+                switch(element.key.type)
                 {
                 case msgpack::type::object_type::STR:
                 {
@@ -25,7 +25,7 @@ namespace Tensile
                 case msgpack::type::object_type::POSITIVE_INTEGER:
                 {
                     auto iKey = element.key.as<uint32_t>();
-                    key = std::to_string(iKey);
+                    key       = std::to_string(iKey);
                     break;
                 }
                 default:
@@ -39,32 +39,36 @@ namespace Tensile
     }
 
     template <typename MyProblem, typename MySolution>
-    std::shared_ptr<SolutionLibrary<MyProblem, MySolution>> MessagePackLoadLibraryFile(std::string const& filename)
+    std::shared_ptr<SolutionLibrary<MyProblem, MySolution>>
+        MessagePackLoadLibraryFile(std::string const& filename)
     {
-        std::ifstream in(filename, std::ios::in | std::ios::binary);
-        std::vector<uint8_t> data((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+        std::ifstream        in(filename, std::ios::in | std::ios::binary);
+        std::vector<uint8_t> data((std::istreambuf_iterator<char>(in)),
+                                  std::istreambuf_iterator<char>());
 
         return MessagePackLoadLibraryData<MyProblem, MySolution>(data);
     }
 
     template <typename MyProblem, typename MySolution>
-    std::shared_ptr<SolutionLibrary<MyProblem, MySolution>> MessagePackLoadLibraryData(std::vector<uint8_t> const& data)
+    std::shared_ptr<SolutionLibrary<MyProblem, MySolution>>
+        MessagePackLoadLibraryData(std::vector<uint8_t> const& data)
     {
         std::shared_ptr<MasterSolutionLibrary<MyProblem, MySolution>> rv;
 
-        auto result = msgpack::unpack((const char *) data.data(), data.size());
+        auto result = msgpack::unpack((const char*)data.data(), data.size());
         Serialization::MessagePackInput min(result.get());
 
-        Serialization::PointerMappingTraits<Tensile::MasterContractionLibrary, Serialization::MessagePackInput>::mapping(min, rv);
+        Serialization::PointerMappingTraits<Tensile::MasterContractionLibrary,
+                                            Serialization::MessagePackInput>::mapping(min, rv);
 
         return rv;
     }
 
-    template
-    std::shared_ptr<SolutionLibrary<ContractionProblem, ContractionSolution>>
-    MessagePackLoadLibraryFile<ContractionProblem, ContractionSolution>(std::string const& filename);
+    template std::shared_ptr<SolutionLibrary<ContractionProblem, ContractionSolution>>
+        MessagePackLoadLibraryFile<ContractionProblem, ContractionSolution>(
+            std::string const& filename);
 
-    template
-    std::shared_ptr<SolutionLibrary<ContractionProblem, ContractionSolution>>
-    MessagePackLoadLibraryData<ContractionProblem, ContractionSolution>(std::vector<uint8_t> const& data);
+    template std::shared_ptr<SolutionLibrary<ContractionProblem, ContractionSolution>>
+        MessagePackLoadLibraryData<ContractionProblem, ContractionSolution>(
+            std::vector<uint8_t> const& data);
 }
